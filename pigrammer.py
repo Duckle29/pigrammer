347e50e9eed5b85c9025a497c8eb64dcaea5ba76
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-08-05 20:22:27 +0200

    Changed font

--- pigrammer.py
+++ pigrammer.py
@@ -66,7 +66,7 @@ bottom = height-padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-font_size = 14
+font_size = 10
 font = ImageFont.truetype('8-bit-fortress.ttf', font_size)
 
 #### End of display stuff
