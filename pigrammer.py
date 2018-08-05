5f411aa603be73b3e4c3581c32a8f55ca8ea86de
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-08-05 19:46:48 +0200

    made lines global

--- pigrammer.py
+++ pigrammer.py
@@ -147,6 +147,7 @@ def signal_handler(sig, frame):
 	sys.exit(0)
 
 def flash_handler(channel):
+	global lines
 	print("Trying to flash")
 	try:
 		flash(avrdude_path, bootloader_hex,log_file,ext_fuse,high_fuse,low_fuse,lock_fuse, avrdude_timeout)
@@ -165,7 +166,7 @@ signal.signal(signal.SIGINT, signal_handler)
 GPIO.add_event_detect(pin_button, GPIO.FALLING, callback=flash_handler, bouncetime=10)
 
 print("Startig loop")
- 
+lines = ["Ready to flash", "Place probe on board", "Push button to flash"]
 while True:
 	drawScreen(x, image, lines)
 	time.sleep(0.5)
