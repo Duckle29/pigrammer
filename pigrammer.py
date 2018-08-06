89d0e00c506d1fcbf2c162880b7daee824d36441
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2018-08-06 18:47:00 +0000

    shortened ready message

--- pigrammer.py
+++ pigrammer.py
@@ -159,13 +159,13 @@ def flash_handler(channel):
 	else:
 		GPIO.output(pin_led_good, GPIO.HIGH)
 		GPIO.output(pin_led_bad, GPIO.LOW)
-		lines = ["Ready to flash", "Place probe on","board. Push", "button to flash"]
+		lines = ["Ready to flash"]
 
 signal.signal(signal.SIGINT, signal_handler)
 GPIO.add_event_detect(pin_button, GPIO.FALLING, callback=flash_handler, bouncetime=1000)
 
 print("Startig loop")
-lines = ["Ready to flash", "Place probe on","board. Push", "button to flash"]
+lines = ["Ready to flash"]
 while True:
 	for idx, line in enumerate(lines):
 		draw.text((x,top+(idx*font_size)), line, font=font, fill=255)
